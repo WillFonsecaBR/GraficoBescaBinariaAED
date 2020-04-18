38192443bcf54d116072a7cbbfcb5c12f5af924f
Willian <willian.alves2109@gmail.com>
2020-04-18 18:14:16 -0300

    Subindo arquivos para o GitHub

---
+++ SegundoTrabalhoAED/Form1.Designer.cs
@@ -0,0 +1,142 @@
+﻿namespace SegundoTrabalho___AED
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.btnPesquisar = new System.Windows.Forms.Button();
+            this.grafico = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.listarProcesso = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnFinalizar = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.grafico)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnPesquisar
+            // 
+            this.btnPesquisar.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnPesquisar.Location = new System.Drawing.Point(32, 2);
+            this.btnPesquisar.Name = "btnPesquisar";
+            this.btnPesquisar.Size = new System.Drawing.Size(170, 89);
+            this.btnPesquisar.TabIndex = 0;
+            this.btnPesquisar.Text = "Iniciar";
+            this.btnPesquisar.UseVisualStyleBackColor = true;
+            this.btnPesquisar.Click += new System.EventHandler(this.btnPesquisar_Click);
+            // 
+            // grafico
+            // 
+            chartArea1.Name = "ChartArea1";
+            this.grafico.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.grafico.Legends.Add(legend1);
+            this.grafico.Location = new System.Drawing.Point(420, 27);
+            this.grafico.Name = "grafico";
+            this.grafico.Palette = System.Windows.Forms.DataVisualization.Charting.ChartColorPalette.SeaGreen;
+            series1.ChartArea = "ChartArea1";
+            series1.Legend = "Legend1";
+            series1.Name = "Series1";
+            this.grafico.Series.Add(series1);
+            this.grafico.Size = new System.Drawing.Size(828, 468);
+            this.grafico.TabIndex = 1;
+            this.grafico.Text = "Gràfico";
+            // 
+            // listarProcesso
+            // 
+            this.listarProcesso.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.listarProcesso.FormattingEnabled = true;
+            this.listarProcesso.ItemHeight = 24;
+            this.listarProcesso.Location = new System.Drawing.Point(12, 131);
+            this.listarProcesso.Name = "listarProcesso";
+            this.listarProcesso.Size = new System.Drawing.Size(386, 364);
+            this.listarProcesso.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(7, 103);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(278, 25);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Informações do Processo";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(652, 2);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(331, 25);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Grafico do Processo de Busca";
+            // 
+            // btnFinalizar
+            // 
+            this.btnFinalizar.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnFinalizar.Location = new System.Drawing.Point(217, 2);
+            this.btnFinalizar.Name = "btnFinalizar";
+            this.btnFinalizar.Size = new System.Drawing.Size(170, 89);
+            this.btnFinalizar.TabIndex = 7;
+            this.btnFinalizar.Text = "Finalizar";
+            this.btnFinalizar.UseVisualStyleBackColor = true;
+            this.btnFinalizar.Click += new System.EventHandler(this.btnFinalizar_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1248, 507);
+            this.Controls.Add(this.btnFinalizar);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.listarProcesso);
+            this.Controls.Add(this.grafico);
+            this.Controls.Add(this.btnPesquisar);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.grafico)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnPesquisar;
+        private System.Windows.Forms.DataVisualization.Charting.Chart grafico;
+        private System.Windows.Forms.ListBox listarProcesso;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnFinalizar;
+    }
+}
+
---
+++ SegundoTrabalhoAED/Form1.cs
@@ -0,0 +1,167 @@
+﻿/**
+ * AED - TRABALHO PRATICO 2 - PESQUISA BINÁRIA
+ * PROFESSOR - ÁLISSON
+ * INTEGRANTES: BERNARDO, GUILHERME BARBOSA, LARYSSA FERNANDA, PEDRO OCT´AVIO, WELLINGTON, WILLIAN ALVES.
+ */
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+
+namespace SegundoTrabalho___AED
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            grafico.Series[0].ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Column;// Modelo os pontos da curva do grafico.
+        }
+        public bool BuscaBI(int num, int[] V) //Busca Binária
+        {
+            int inicio = 0, fim = V.Length - 1, meio = (inicio + fim) / 2;
+
+            while (inicio <= fim && V[meio] != num)
+            {
+                if (num > V[meio])
+                {
+                    inicio = meio + 1;
+                }
+                else
+                {
+                    fim = meio - 1;
+                }
+                meio = (inicio + fim) / 2;
+            }
+
+            if (V[meio] == num)
+                return true;
+            else
+                return false;
+        }
+
+        public void QuickSortRecursivo(int[] vetor, int primeiro, int ultimo) //Método pra ordenar o vetor
+        {
+
+            int baixo, alto, meio, pivo, repositorio;
+            baixo = primeiro;
+            alto = ultimo;
+            meio = (int)((baixo + alto) / 2);
+
+            pivo = vetor[meio];
+
+            while (baixo <= alto)
+            {
+                while (vetor[baixo] < pivo)
+                    baixo++;
+                while (vetor[alto] > pivo)
+                    alto--;
+                if (baixo < alto)
+                {
+                    repositorio = vetor[baixo];
+                    vetor[baixo++] = vetor[alto];
+                    vetor[alto--] = repositorio;
+                }
+                else
+                {
+                    if (baixo == alto)
+                    {
+                        baixo++;
+                        alto--;
+                    }
+                }
+            }
+
+            if (alto > primeiro)
+                QuickSortRecursivo(vetor, primeiro, alto);
+            if (baixo < ultimo)
+                QuickSortRecursivo(vetor, baixo, ultimo);
+        }
+
+        public void preencher(int[] V) //Preencher o vetor
+        {
+            Random r = new Random();
+
+            for (int i = 0; i < V.Length; i++)
+            {
+                V[i] = r.Next(10000, 100000);
+            }
+        }
+        public void Executar() // Inicia o procedimento de preenchimento, ordenação busca e contagem de tempo.
+        {
+            Stopwatch sw = new Stopwatch(); //Salva o tempo de execução
+            TimeSpan tempo; //Variável para receber o tempo de execução
+                            
+
+            int[] x; // Vetor onde sera adicionado os elementos.
+
+            bool aux3; // Se o elemento esta ou não no vetor 
+
+            string aux4; // Variavel que guardará o resultado da condição feita para saber se o elemento esta no vetor.
+
+            // Gera um elemento aleatorio para pesquisa no vetor.
+            Random r = new Random();
+            int aux = r.Next(10000, 100000);
+
+            double aux2;
+
+            for (int i = 10000; i <= 100000; i = i + 1000) //Preenche e faz a busca de vetores de 10.000 a 100.000 posições
+            {
+                x = new int[i];
+                preencher(x);
+
+                QuickSortRecursivo(x, 0, x.Length - 1);// Ordena o vetor antes da pesquisa.
+
+                sw.Start(); //Começa a contar o tempo
+                aux3 = BuscaBI(aux, x); // executa a busca
+                sw.Stop(); //Para de contar o tempo
+
+                tempo = sw.Elapsed;//Recebe o tempo de execução
+
+                aux2 = Convert.ToDouble(tempo.TotalSeconds); // converte o tempo
+
+                // Condição para saber se o elemento esta ou não no vetor.
+                if (aux3 == true)
+                {
+                    aux4 = "Sim";
+                }
+                else
+                {
+                    aux4 = "Não";
+                }
+
+                //Exibe as informações de cada etapa do processo no list box.
+                listarProcesso.Items.Add("=============================");
+                listarProcesso.Items.Add("Quantidade elementos : " + i);
+                listarProcesso.Items.Add("Elemento Procurado: " + aux);
+                listarProcesso.Items.Add("Elemento encontrado? " + aux4);
+                listarProcesso.Items.Add("Tempo decorrido: " + aux2);
+
+                //insere os elementos nos eixos do grafico.
+                grafico.Series[0].Points.AddXY(i, aux2);
+                grafico.Update(); // atualiza os graficos
+            }
+        }
+
+        private void btnPesquisar_Click(object sender, EventArgs e)
+        {
+            btnPesquisar.Visible = false; // Faz o botão ficar invisivel.
+            btnFinalizar.Visible = false;
+            Executar(); // Executa a função de inicialização do sistema. 
+            btnFinalizar.Visible = true;
+        }
+
+        private void btnFinalizar_Click(object sender, EventArgs e)
+        {
+            Close(); // Fecha o programa
+        }
+    }
+}
